2c29617632025e7b0e18d04899078b520ab5504b
Joëlle <joelle.a.m.devries@gmail.com>
2021-06-07 13:33:15 +0200

    1

--- RepeaterNode/4_LoRa_Receiver/CommunicationProtocol.h
+++ RepeaterNode/4_LoRa_Receiver/CommunicationProtocol.h
@@ -1,11 +1,8 @@
 
 bool CheckParity(uint8_t data);
-bool CheckID(uint8_t[] data);
+bool CheckID(uint8_t data[], uint8_t savedID[]);
 uint8_t GetVolt(uint8_t data);
 uint8_t GetAmp(uint8_t data);
 uint16_t GetBatteryLevel(uint8_t data);
 uint16_t GetSensorData(uint8_t data);
 uint8_t GetChecksum(uint8_t data);
-
-
-
--- RepeaterNode/4_LoRa_Receiver/CommunicationProtocol.ino
+++ RepeaterNode/4_LoRa_Receiver/CommunicationProtocol.ino
@@ -7,7 +7,7 @@ bool CheckParity(uint8_t data)
 }
 
 //returns true if the the value has not been seen before and the message is free to use
-bool CheckID(uint8_t[] data, uint8_t[] savedID)
+bool CheckID(uint8_t data[], uint8_t savedID[])
 {
   uint8_t id = data[0]; 
   for (int i = 0; i < sizeof(savedID); i ++)
@@ -20,27 +20,31 @@ bool CheckID(uint8_t[] data, uint8_t[] savedID)
   return true;
 }
 
-uint8_t GetVolt(uint8_t data)
+uint8_t GetVolt(uint8_t data[])
 {
   return data[1]; 
 }
 
-uint8_t GetAmp(uint8_t data)
+uint8_t GetAmp(uint8_t data[])
 {
    return data[2]; 
 }
 
-uint16_t GetBatteryLevel(uint8_t data)
+uint16_t GetBatteryLevel(uint8_t data[])
 {
   return data[3] + data[4]; 
 }
 
-uint16_t GetSensorData(uint8_t data)
+uint16_t GetSensorData(uint8_t data[])
 {
-  return data[5] + data[6]; 
+  uint16_t sensor = 0;
+  sensor = data[5];
+  sensor = sensor << 8;
+  sensor |= data[6];
+  return sensor; 
 }
 
-uint8_t GetChecksum(uint8_t data)
+uint8_t GetChecksum(uint8_t data[])
 {
   
 }
