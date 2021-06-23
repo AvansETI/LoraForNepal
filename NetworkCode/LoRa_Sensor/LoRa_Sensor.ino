43e8fb2998182f1bf8d05a6b260f02c958b110ad
phunderr <pm.holthuijsen@student.avans.nl>
2021-06-23 11:42:22 +0200

    [added] SensorCode

--- NetworkCode/LoRa_Sensor/LoRa_Sensor.ino
+++ NetworkCode/LoRa_Sensor/LoRa_Sensor.ino
@@ -34,6 +34,8 @@
 
 SX128XLT LT;                                     //create a library class instance called LT
 
+const int pingPin = 10;
+
 uint32_t RXpacketCount;
 uint32_t errors;
 
@@ -57,6 +59,33 @@ void loop()
   timer.tick();
 
 //TODO insert SENSOR code
+ uint16_t duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  pinMode(6, OUTPUT);
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+
+
    
 }
 
@@ -260,6 +289,8 @@ void led_Flash(uint16_t flashes, uint16_t delaymS)
 
 void setup()
 {
+
+  
   
   pinMode(LED1, OUTPUT);                        //setup pin as output for indicator LED
   led_Flash(2, 125);                            //two quick LED flashes to indicate program start
